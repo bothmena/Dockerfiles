570b4a1d9c7376265c061507add8f9292323829a
Aymen Ben Othmen <aymenbenothmenabo@gmail.com>
2019-03-09 21:38:31 +0100

    tensorflow+keras docker

---
+++ tensorflow/jupyter_notebook_config.py
@@ -0,0 +1,11 @@
+import os
+
+
+c = c  # pylint:disable=undefined-variable
+c.NotebookApp.ip = '0.0.0.0'  # https://github.com/jupyter/notebook/issues/3946
+c.NotebookApp.port = int(os.getenv('PORT', 8888))
+c.NotebookApp.open_browser = False
+
+c.NotebookApp.password = ''
+c.NotebookApp.token = ''
+
